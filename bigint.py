fc3ceda141451da5adee28ac1f7103b15062d40f
Mathieu Bordere <mathieu@letmetweakit.com>
2025-02-07 17:56:55 +0100

    f

--- bigint.py
+++ bigint.py
@@ -1,24 +1,43 @@
 import base64
+import math
 import sys
 
 def main():
-    # 1 operation type and 2 inputs
-    assert(len(sys.argv) == 4)
-    op = sys.argv[1]
-    assert(op == 'add' or op == 'mult')
-    lb = base64.b64decode((sys.argv[2]))
-    rb = base64.b64decode((sys.argv[3]))
-    print("{} {}".format(lb, rb))
-    l = int.from_bytes(lb)
-    r = int.from_bytes(rb)
-    print("{} {}".format(l, r))
-    match op:
-        case 'add':
-            res = l + r
-        case 'mult':
-            res = l * r
-    print(int(res))
-    sys.stdout.buffer.write(base64.b64encode(res.to_bytes()))
+    assert(len(sys.argv) == 1)
+    # The program waits for input from the user, the first line is the command, followed
+    # by the arguments. The program will then execute the command and write the result to stdout.
+    # The possible commands are: 'add', 'mult' and 'stop'. When the command 'stop' is given, the program
+    # terminates. The program will only accept commands that are in the list of possible commands.
+    # 'add' and 'mult' will take two arguments, which are two base64 encoded integers. The program will
+    # decode the integers, perform the operation and encode the result. The result will be written to stdout.
+    while True:
+        command = sys.stdin.readline().strip()
+        if command == 'stop':
+            break
+        elif command == 'add':
+            a = sys.stdin.readline().strip()
+            b = sys.stdin.readline().strip()
+            a = int.from_bytes(base64.b64decode(a))
+            b = int.from_bytes(base64.b64decode(b))
+            result = a + b
+            result = int.to_bytes(result, math.ceil(result.bit_length() / 8), 'big')
+            result = base64.b64encode(result)
+            sys.stdout.buffer.write(result)
+            sys.stdout.buffer.write(b'\n')
+            sys.stdout.flush()
+        elif command == 'mult':
+            a = sys.stdin.readline().strip()
+            b = sys.stdin.readline().strip()
+            a = int.from_bytes(base64.b64decode(a))
+            b = int.from_bytes(base64.b64decode(b))
+            result = a * b
+            result = int.to_bytes(result, math.ceil(result.bit_length() / 8), 'big')
+            result = base64.b64encode(result)
+            sys.stdout.buffer.write(result)
+            sys.stdout.buffer.write(b'\n')
+            sys.stdout.flush()
+        else:
+            break
 
 if __name__ == "__main__":
     main()
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,64 @@
-use std::io::{self, Write};
-use std::process::Command;
+use base64::decode;
+use num_bigint::BigUint;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Read, Result, Write};
+use std::process::{Command, Stdio};
 
-fn main() {
-    let output = Command::new("python")
+fn main() -> Result<()> {
+    let mut rng = File::open("/dev/urandom")?;
+
+    /* Randomly decide how many bytes to generate */
+    let mut buffer = [0u8; 1];
+    rng.read_exact(&mut buffer)?;
+
+    let mut n_bytes = u8::from_be_bytes(buffer);
+    if n_bytes < 2 {
+        n_bytes = 2;
+    }
+    let mut buffer2: Vec<u8> = Vec::with_capacity(n_bytes as usize);
+    buffer2.resize(n_bytes as usize, 0);
+    rng.read_exact(&mut buffer2)?;
+    let mid = n_bytes as usize / 2;
+    println!(
+        "{} {} {:?} {}",
+        buffer2.len(),
+        buffer2.capacity(),
+        n_bytes,
+        mid
+    );
+    let split = buffer2.split_at(mid);
+
+    let a = BigUint::from_bytes_be(split.0);
+    let b = BigUint::from_bytes_be(split.1);
+    let res = &a * &b;
+    //println!("{}", res);
+    let res_encoded = res.to_bytes_be();
+
+    let mut bigintpy = Command::new("python3")
         .arg("bigint.py")
-        .output()
-        .expect("failed to execute process");
+        .stdout(Stdio::piped()) // Capture stdout
+        .stdin(Stdio::piped()) // Capture stdin
+        .spawn()
+        .expect("Failed to start process");
 
-    println!("status: {}", output.status);
-    io::stdout().write_all(&output.stdout).unwrap();
-    io::stderr().write_all(&output.stderr).unwrap();
+    if let Some(mut stdin) = bigintpy.stdin.take() {
+        writeln!(stdin, "mult").unwrap();
+        writeln!(stdin, "{}", base64::encode(&a.to_bytes_be())).unwrap();
+        writeln!(stdin, "{}", base64::encode(&b.to_bytes_be())).unwrap();
+    }
 
-    assert!(output.status.success());
+    if let Some(stdout) = bigintpy.stdout.take() {
+        let reader = BufReader::new(stdout);
+        for line in reader.lines().take(1) {
+            match line {
+                Ok(text) => {
+                    let decoded = decode(text).unwrap();
+                    assert!(decoded == res_encoded);
+                }
+                Err(e) => eprintln!("Error reading line: {}", e),
+            }
+        }
+    }
+
+    Ok(())
 }
